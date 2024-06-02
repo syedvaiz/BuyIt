c0c548df23b1707267ecb9e8728afba65a135912
syedvaiz <44797772+syedvaiz@users.noreply.github.com>
2024-06-02 19:59:38 +0530

    Update ShopCategory.jsx

--- e-commerce-frontend/src/Pages/ShopCategory.jsx
+++ e-commerce-frontend/src/Pages/ShopCategory.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./CSS/ShopCategory.css";
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
 import Item from "../Components/Item/Item";
 import { Link } from "react-router-dom";
 
@@ -10,13 +10,13 @@ const ShopCategory = (props) => {
 
   const fetchInfo = () => { 
     fetch('https://buyit-api-theta.vercel.app/allproducts') 
-            .then((res) => res.json()) 
-            .then((data) => setAllProducts(data))
-    }
+      .then((res) => res.json()) 
+      .then((data) => setAllProducts(data));
+  };
 
-    useEffect(() => {
-      fetchInfo();
-    }, [])
+  useEffect(() => {
+    fetchInfo();
+  }, []);
     
   return (
     <div className="shopcategory">
@@ -27,18 +27,24 @@ const ShopCategory = (props) => {
       </div>
       <div className="shopcategory-products">
         {allproducts.map((item,i) => {
-            if(props.category===item.category)
-            {
-              return <Item id={item.id} key={i} name={item.name} image={item.image}  new_price={item.new_price} old_price={item.old_price}/>;
-            }
-            else
-            {
-              return null;
-            }
+          if(props.category === item.category) {
+            return (
+              <Item 
+                id={item.id} 
+                key={i} 
+                name={item.name} 
+                image={item.image}  
+                new_price={item.new_price} 
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
         })}
       </div>
       <div className="shopcategory-loadmore">
-      <Link to='/' style={{ textDecoration: 'none' }}>Explore More</Link>
+        <Link to='/' style={{ textDecoration: 'none' }}>Explore More</Link>
       </div>
     </div>
   );

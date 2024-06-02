744ef396ff8cfd43b5e9fbf5e5d9bdb11da65869
syedvaiz <44797772+syedvaiz@users.noreply.github.com>
2024-06-02 17:07:56 +0530

    updated connection

--- e-commerce-frontend/src/Pages/LoginSignup.jsx
+++ e-commerce-frontend/src/Pages/LoginSignup.jsx
@@ -12,7 +12,7 @@ const LoginSignup = () => {
 
   const login = async () => {
     let dataObj;
-    await fetch('https://buyit-api-theta.vercel.app/login', {
+    await fetch('https://buyit-api-theta.vercel.app', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',

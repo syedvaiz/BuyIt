0efde0d330051808614030e8fd6696570c09985a
syedvaiz <44797772+syedvaiz@users.noreply.github.com>
2024-06-02 16:35:23 +0530

    updated connection

--- e-commerce-frontend/src/Pages/Shop.jsx
+++ e-commerce-frontend/src/Pages/Shop.jsx
@@ -11,10 +11,10 @@ const Shop = () => {
   const [newcollection, setNewCollection] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/popularinwomen') 
+    fetch('https://buyit-api-theta.vercel.app/popularinwomen') 
             .then((res) => res.json()) 
             .then((data) => setPopular(data))
-    fetch('http://localhost:4000/newcollections') 
+    fetch('https://buyit-api-theta.vercel.app/newcollections') 
             .then((res) => res.json()) 
             .then((data) => setNewCollection(data))
     }

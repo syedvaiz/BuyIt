5e278c28d3ba65cfdad64e6feca1180a2b38a24b
syedvaiz <44797772+syedvaiz@users.noreply.github.com>
2024-06-02 16:36:26 +0530

    updated connection

--- e-commerce-frontend/src/Pages/ShopCategory.jsx
+++ e-commerce-frontend/src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://buyit-api-theta.vercel.app/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }

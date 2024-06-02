5be215c70d04d36a0bf7dfb2912229737f5b33f4
syedvaiz <44797772+syedvaiz@users.noreply.github.com>
2024-06-02 19:53:30 +0530

    Update Shop.jsx

--- e-commerce-frontend/src/Pages/Shop.jsx
+++ e-commerce-frontend/src/Pages/Shop.jsx
@@ -1,38 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import Hero from '../Components/Hero/Hero'
-import Popular from '../Components/Popular/Popular'
-import Offers from '../Components/Offers/Offers'
-import NewCollections from '../Components/NewCollections/NewCollections'
-import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import React, { useEffect, useState } from 'react';
+import Hero from '../Components/Hero/Hero';
+import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
 
 const Shop = () => {
-
   const [popular, setPopular] = useState([]);
   const [newcollection, setNewCollection] = useState([]);
 
   const fetchInfo = () => { 
     fetch('https://buyit-api-theta.vercel.app/popularinwomen') 
-            .then((res) => res.json()) 
-            .then((data) => setPopular(data))
+      .then((res) => res.json()) 
+      .then((data) => setPopular(data));
+    
     fetch('https://buyit-api-theta.vercel.app/newcollections') 
-            .then((res) => res.json()) 
-            .then((data) => setNewCollection(data))
-    }
+      .then((res) => res.json()) 
+      .then((data) => setNewCollection(data));
+  }
 
-    useEffect(() => {
-      fetchInfo();
-    }, [])
-
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
   return (
     <div>
-      <Hero/>
-      <Popular data={popular}/>
-      <Offers/>
-      <NewCollections data={newcollection}/>
-      <NewsLetter/>
+      <Hero />
+      <Popular data={popular} />
+      <Offers />
+      <NewCollections data={newcollection} />
+      <NewsLetter />
     </div>
-  )
+  );
 }
 
-export default Shop
+export default Shop;
